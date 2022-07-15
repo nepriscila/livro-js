09067bf1f484b6f770947dc06d9fa235095d0d77
nepriscila <neyleia16@hotmail.com>
2022-07-15 14:38:44 -0300

    ajuste de código

--- livro-js/cap02/js/ex2_a.js
+++ livro-js/cap02/js/ex2_a.js
@@ -1,17 +1,17 @@
-// Referência ao form e ao elemento h3 (onde será exibida a resposta)
+
 const frm = document.querySelector("form")
 const resp1 = document.querySelector("#outResp1")
 const resp2 = document.querySelector("#outResp2")
 
-// cria um "ouvinte" do evento quando o botão submit for clicado
+
 frm.addEventListener("submit", (e) => {
-    const medicamento = frm.inMedicamento.value // obtém o contéudo das caixas
+    const medicamento = frm.inMedicamento.value 
     const preco = Number(frm.inPreço.value)
 
-    const promocao = Math.floor(preco*2) // calcula o preço com o desconto
+    const promocao = Math.floor(preco*2-1) 
 
     resp1.innerText = `Promoção de: ${medicamento}`
-    resp2.innerText = `Leve 2 por apenas R$: ${promocao.toFixed(2)}` //exibe a resposta
+    resp2.innerText = `Leve 2 por apenas R$: ${promocao.toFixed(2)}` 
     
     e.preventDefault()
 })
